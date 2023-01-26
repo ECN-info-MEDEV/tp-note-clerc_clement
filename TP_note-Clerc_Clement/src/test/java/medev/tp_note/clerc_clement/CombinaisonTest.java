f5c223767fb8aa733d69b2127925728dada88e5a
Leo-Clerc <leocoursadistance@gmail.com>
2023-01-26 10:28:54 +0100

    Tests are working

--- TP_note-Clerc_Clement/src/test/java/medev/tp_note/clerc_clement/CombinaisonTest.java
+++ TP_note-Clerc_Clement/src/test/java/medev/tp_note/clerc_clement/CombinaisonTest.java
@@ -1,17 +1,19 @@
 package medev.tp_note.clerc_clement;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 class CombinaisonTest {
-    Couleur rouge = new couleur(0);
-    Couleur bleu = new couleur(1);
-    Combinaison aDeviner = new Combinaison(rouge, rouge, rouge,rouge);
-    Combinaison tentative1 = new Combinaison(bleu, rouge, rouge, rouge);
-    Combinaison tentative2 = new Combinaison(rouge, rouge, rouge, rouge)
-  @test
+    private Couleur rouge = new Couleur(0);
+    private Couleur bleu = new Couleur(1);
+    private Combinaison aDeviner = new Combinaison(rouge, rouge, rouge,rouge);
+    private Combinaison tentative1 = new Combinaison(bleu, rouge, rouge, rouge);
+    private Combinaison tentative2 = new Combinaison(rouge, rouge, rouge, rouge);
+  @Test
     void egalite(){
       assertEquals(false, aDeviner.compare(tentative1));
   }
-  @test
+  @Test
     void inegalite(){
         assertEquals(true, aDeviner.compare(tentative2));
   }
